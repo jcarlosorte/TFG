5bb9ef4ab0da6c777e4c6c0e8c1e5b3db1d83054
jcarlosorte <jcarlosorte@ugr.es>
2019-05-03 15:25:08 +0200

    roc funcionando

--- particion-fcv.py
+++ particion-fcv.py
@@ -32,8 +32,8 @@ def Porcentaje(X,Y):
 
 folds = 5
 runs = 1
-DataSet = ['Fox_scaled','Musk2_scaled']#pruebas
-#DataSet = ['musk1_scaled','Musk2_scaled','Elephant_scaled','Fox_scaled','mutagenesis1_scaled','mutagenesis2_scaled','Tiger_scaled']
+#DataSet = ['Fox_scaled','Musk2_scaled']#pruebas
+DataSet = ['musk1_scaled','Musk2_scaled','Elephant_scaled','Fox_scaled','mutagenesis1_scaled','mutagenesis2_scaled','Tiger_scaled']
 carpeta = '../dataNoisy/'
 filename1 = 'X_train_bags.csv'
 filename2 = 'Y_train_labels.csv'
@@ -44,20 +44,33 @@ NoisyPercent = [0,5,10,15,20,25,30]
 
 f = open("../TestNoisy.txt", "w+")       
 for j in DataSet:
-    resul = [[],[],[],[],[],[],[]]
+    resul1 = [[],[],[],[],[],[],[]]
     resul2 = [[],[],[],[],[],[],[]]
     resul3 = [[],[],[],[],[],[],[]]
-    SMILaMax = [simpleMIL(),{'type': 'max'},'MIL max',resul]
-    SMILaMin = [simpleMIL(),{'type': 'min'},'MIL min',resul2]
-    SMILaExt = [simpleMIL(),{'type': 'extreme'},'MIL Extreme',resul3]
-    
-    Clasificadores = [SMILaMax,SMILaMin,SMILaExt]
-    for h,clasi in enumerate(Clasificadores):
-        print('Clasificador vacio: '+str(clasi[2]))
-        for p,noy in enumerate(NoisyPercent):
-            print('\t=>Ruido: '+str(noy)+'%\tPrecisión Media: '+str(np.mean(clasi[3][p])))
-    
-    
+    resul4 = [[],[],[],[],[],[],[]]
+    resul5 = [[],[],[],[],[],[],[]]
+    resul6 = [[],[],[],[],[],[],[]]
+    resul7 = [[],[],[],[],[],[],[]]
+    resul8 = [[],[],[],[],[],[],[]]
+    roc_m_1 = [[],[],[],[],[],[],[]]
+    roc_m_2 = [[],[],[],[],[],[],[]]
+    roc_m_3 = [[],[],[],[],[],[],[]]
+    roc_m_4 = [[],[],[],[],[],[],[]]
+    roc_m_5 = [[],[],[],[],[],[],[]]
+    roc_m_6 = [[],[],[],[],[],[],[]]
+    roc_m_7 = [[],[],[],[],[],[],[]]
+    roc_m_8 = [[],[],[],[],[],[],[]]
+    SMILaMax = [simpleMIL(),{'type': 'max'},'MIL max',resul1,roc_m_1]
+    SMILaMin = [simpleMIL(),{'type': 'min'},'MIL min',resul2,roc_m_2]
+    SMILaExt = [simpleMIL(),{'type': 'extreme'},'MIL Extreme',resul3,roc_m_3]
+    BOW_clas = [BOW(),{'k':100,'covar_type':'diag','n_iter':20},'BOW',resul4,roc_m_4]
+    CKNN_cla = [CKNN(),{'references': 3, 'citers': 5},'CKNN',resul5,roc_m_5]
+    maxDD_cl = [maxDD(),{},'DIVERSE DENSITY',resul6,roc_m_6]
+    EMDD_cla = [EMDD(),{},'EM-DD',resul7,roc_m_7]
+    MILB_cla = [MILBoost(),{},'MILBOOST',resul8,roc_m_8]
+ 
+#    Clasificadores = [SMILaMax,SMILaMin,SMILaExt]
+    Clasificadores = [SMILaMax,SMILaMin,SMILaExt,BOW_clas,CKNN_cla,maxDD_cl,EMDD_cla,MILB_cla]
     print '\n********** DATASET: ',j,' **********\n'
     bags,labels,X = load_data(j)
     bags,labels = shuffle(bags, labels, random_state=rand.randint(0, 100))
@@ -101,6 +114,7 @@ for j in DataSet:
                 accuracie = (100 * np.average(Y_test.T == np.sign(predictions)))
                 auc_score = (100 * roc_auc_score(Y_test,predictions))  
                 Clasificadores[i][3][ny].append(accuracie)
+                Clasificadores[i][4][ny].append(auc_score)
 #                print('Clasificador :'+str(cl[2])+'\n\t Ruido '+str(k)+'%\n\t Precisión: '+ str(auc_score))
                 
             #============================================
@@ -131,7 +145,7 @@ for j in DataSet:
         print('Clasificador: '+str(clasi[2]))
         f.write('Clasificador: '+str(clasi[2])+'\n')
         for p,noy in enumerate(NoisyPercent):
-            print('\t=>Ruido: '+str(noy)+'%\tPrecisión Media: '+str(np.mean(clasi[3][p])))
-            f.write('\t=>Ruido: '+str(noy)+'%\tPrecisión Media: '+str(np.mean(clasi[3][p]))+'\n')
+            print('\t=>Ruido: '+str(noy)+'%\tPrecisión Media: '+str(np.mean(clasi[3][p]))+'\n\t\t\tMedia Roc Score: '+str(np.mean(clasi[4][p])))
+            f.write('\t=>Ruido: '+str(noy)+'%\tPrecisión Media: '+str(np.mean(clasi[3][p]))+'\n\t\t\tMedia Roc Score: '+str(np.mean(clasi[4][p]))+'\n')
 
 f.close()
